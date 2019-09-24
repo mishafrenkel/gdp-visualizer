83bb2ea538e5c8b334bdc5c26145a1e4f347f451
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-23 21:36:29 -0700

    update for Heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const favicon = require('express-favicon');
+
 var app = express();
 var axios = require('axios');
 var cors = require('cors');
@@ -6,19 +8,21 @@ var path = require('path');
 const bodyParser = require('body-parser');
 let data = []
 
-//use cors to allow cross origin resource sharing
+// use cors to allow cross origin resource sharing
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public'));
 });
 
 app.get('/api/graph', function (req, res) {
@@ -40,6 +44,6 @@ axios.get('http://api.worldbank.org/countries/USA/indicators/NY.GDP.MKTP.CD?per_
     })
     .then(_ => {
         app.listen(process.env.PORT || 3001)
-        console.log("listening on", process.env.PORT)
+        console.log("listening on", process.env.PORT || 3001);
     })
     .catch(e => console.error(e));
